cec637fb35a8d27f5267dba91995171a27b079fb
PhamTrongPhung20182723 <phung.pt182723@sis.hust.edu.vn>
2022-02-13 17:01:31 +0700

    commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace deparseTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBoxSMS = new System.Windows.Forms.TextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(455, 377);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 42);
+            this.button1.TabIndex = 121;
+            this.button1.Text = "Disconnect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(455, 323);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 39);
+            this.button2.TabIndex = 120;
+            this.button2.Text = "Connect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(230, 31);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(148, 25);
+            this.label5.TabIndex = 125;
+            this.label5.Text = "SMS Banking:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(230, 323);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 25);
+            this.label2.TabIndex = 124;
+            this.label2.Text = "COM :";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(235, 376);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(138, 39);
+            this.comboBox1.TabIndex = 123;
+            // 
+            // textBoxSMS
+            // 
+            this.textBoxSMS.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSMS.Location = new System.Drawing.Point(235, 70);
+            this.textBoxSMS.Multiline = true;
+            this.textBoxSMS.Name = "textBoxSMS";
+            this.textBoxSMS.ReadOnly = true;
+            this.textBoxSMS.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxSMS.Size = new System.Drawing.Size(352, 227);
+            this.textBoxSMS.TabIndex = 122;
+            this.textBoxSMS.Text = "TK 9846573836482\r\nSo tien GD: +50,000\r\nSo du: 18,000,000\r\nNoi dung GD: 20182723_P" +
+    "ointTable\r\nSo GD: 1248563756245\r\n17:33:24";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBoxSMS);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1Closed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBoxSMS;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+
+
+
+namespace deparseTool
+{
+    public partial class Form1 : Form
+    {
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "euXIwSf1qoXdcqULpMr2sGekOD3hC6UzPLUhUnbi",
+            BasePath = "https://tsdv-oop-default-rtdb.firebaseio.com/"
+        };
+
+        IFirebaseClient client;
+
+        string InputData;
+        delegate void SetTextCallback(string text);
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort1.DataReceived += new SerialDataReceivedEventHandler(DataReceive);
+        }
+        private void DataReceive(object obj, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                Thread.Sleep(500);
+                InputData = serialPort1.ReadExisting();
+                if (InputData != null)
+                {
+                    this.BeginInvoke(new SetTextCallback(SetText), new object[] { InputData });
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Close Serial Port!");
+            }
+        }
+        private void SetText(string text)
+        {
+            textBoxSMS.Text = InputData.Trim();
+            if (textBoxSMS.Text != null) {
+                deparseSMS(textBoxSMS.Text);
+            }
+        }
+
+        private void deparseSMS(string text) {
+            int index = text.IndexOf("ND");
+            int index2 = text.IndexOf("GD +");
+            int index3 = text.IndexOf("DV");
+            string ID = text.Substring(index, index+9);
+            string fee = text.Substring(index2, index2 + 6);
+            string service = text.Substring(index3, index3 + 3);
+            switch (service)
+            {
+                case "DV1":
+                    if (fee == "50000")
+                    {
+                        SetResponse res = client.Set(@"PointTable/" + ID + "/PayState", "OK");
+                    }
+                    else
+                    {
+                        SetResponse res = client.Set(@"PointTable/" + ID + "/PayState", fee);
+                    }
+                    break;
+
+                case "DV2":
+                    if (fee == "50000")
+                    {
+                        SetResponse res = client.Set(@"CheckExam/" + ID + "/PayState", "OK");
+                    }
+                    else
+                    {
+                        SetResponse res = client.Set(@"CheckExam/" + ID + "/PayState", fee);
+                    }
+                    break;
+
+                case "DV3":
+                    if (fee == "50000")
+                    {
+                        SetResponse res = client.Set(@"RegisExpClass/" + ID + "/PayState", "OK");
+                    }
+                    else
+                    {
+                        SetResponse res = client.Set(@"RegisExpClass/" + ID + "/PayState", fee);
+                    }
+                    break;
+            }
+           
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!serialPort1.IsOpen)
+                {
+                    serialPort1.PortName = comboBox1.Text;
+                    serialPort1.BaudRate = Convert.ToInt32(9600);
+                    serialPort1.Open();
+                }
+
+            }
+            catch (Exception exception)
+            {
+                if (!serialPort1.IsOpen)
+                {
+                    MessageBox.Show("Serial port is not connected");
+                }
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new FireSharp.FirebaseClient(config);
+            comboBox1.DataSource = SerialPort.GetPortNames();
+        }
+        private void Form1Closed(object sender, FormClosedEventArgs e)
+        {
+            serialPort1.Close();
+        }
+    }
+}
